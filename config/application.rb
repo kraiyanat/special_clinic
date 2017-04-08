5c666dab221dfc5d4ad7145080c8ddb989d12188
Kraiyanat Tee <tee@iwa.fi>
2017-04-08 11:32:53 +0700

    Renamed project to special_clinic

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rails5PgRspec
+module SpecialClinic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
